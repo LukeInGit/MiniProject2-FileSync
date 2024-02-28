0f47d741a0926a436675144b31e6e5e76b85b982
LukeInGit <LukeElias@protonmail.com>
2024-02-28 14:44:32 +0000

    Implemented filename comparisons.
    Added bool to directoryiterator class to identify the main directory.

--- main.cpp
+++ main.cpp
@@ -5,28 +5,40 @@ class DirectoryIterator
 {
 private:
     std::filesystem::directory_iterator m_iterator{};
-    std::string m_path{};
+    std::filesystem::path m_path{};
+    bool m_isMainDirectory{};
 
     DirectoryIterator() = delete;                                           // prevent default constructer usage
     DirectoryIterator(const DirectoryIterator&) = delete;                   // delete copy constructor
     DirectoryIterator& operator=(const DirectoryIterator&) = delete;        // delete assignment constructor
 public:
 
-    explicit DirectoryIterator(std::string filepath)                        // must provide a string when initialising
-        :m_iterator{ filepath }, m_path{ filepath }
+    explicit DirectoryIterator(std::string filepath, bool isMain=false)    
+        :m_iterator{ filepath }, m_path{ filepath }, m_isMainDirectory{isMain}
     {}
-
-    const std::filesystem::directory_iterator& GetIterator() const
-    {
-        return m_iterator;
-    }
 
     void ResetIterator()
     {
         m_iterator = std::filesystem::directory_iterator{ m_path };
     }
 
-    void CheckDirectory()                                                   // print out filenames in directory
+    const std::filesystem::directory_iterator& GetIterator() const
+    {
+        return m_iterator;
+    }
+
+    const std::filesystem::path& GetPath() const
+    {
+        return m_path;
+    }
+
+    bool IsMainDirectory()
+    {
+        return m_isMainDirectory;
+    }
+
+
+    void CheckDirectory()      // print out filenames in directory
     {
 
         for (const auto& entry : m_iterator)
@@ -38,22 +50,63 @@ public:
         }
         ResetIterator();
     }
+
+
+
 };
 
-void basicloop(DirectoryIterator& iterator)
+bool IsFileInDirectory(const std::string& filename, const std::filesystem::path& directory)         // check if filename is in target directory
 {
-    while (true)
-    {
-        iterator.CheckDirectory();
+    std::filesystem::path targetFilePath = directory / filename;
+    return std::filesystem::exists(targetFilePath);
+}
+
+void SyncDirectories( DirectoryIterator& sourceDirectory,  DirectoryIterator& targetDirectory) { // compare all filenames between two directories, call relevant copying functions-
+    for (const auto& entry : sourceDirectory.GetIterator()) {                                    // depending on if filenames exist.
+                                                                                                 // different usage if directory is main directory - not ideal - may change
+        std::string filename = entry.path().filename().string();   
+
+
+        if (IsFileInDirectory(filename, targetDirectory.GetPath())) {
+
+                std::cout << filename << " exists in target directory.\n";
+        }
+        else {
+            if (sourceDirectory.IsMainDirectory())
+            {
+                std::cout << filename << " does not exist in target directory. Copying...\n";
+            }
+            else
+            {
+                std::cout << filename << " does not exist in main directory. Deleting...\n";
+            }
+        }
+    }
+    sourceDirectory.ResetIterator();    // iterators need to be reset or they will only detect the final file
+    targetDirectory.ResetIterator();
+}
+
+
+void basicloop(DirectoryIterator& sourceDirectory, DirectoryIterator& targetDirectory) //temporary loop
+{
+    while (true) {
+        SyncDirectories(sourceDirectory, targetDirectory);
+        SyncDirectories(targetDirectory, sourceDirectory);
+
     }
 }
 
 int main()
 {
 
-    DirectoryIterator bob{ "C:/Users/Luke/source/repos/MiniProject_2_FileSync/CopyFrom" };
+    DirectoryIterator sourceDirectory{ "C:/Users/Luke/source/repos/MiniProject_2_FileSync/DirectoryA",true };
+    DirectoryIterator targetDirectory{ "C:/Users/Luke/source/repos/MiniProject_2_FileSync/DirectoryB"};
+    DirectoryIterator binDirectory{ "C:/Users/Luke/source/repos/MiniProject_2_FileSync/DirectoryBin"};
 
-    basicloop(bob);
+    SyncDirectories(sourceDirectory, targetDirectory);
+    SyncDirectories(targetDirectory, sourceDirectory);
+
+    //basicloop(sourceDirectory, targetDirectory);
 
 
     return 0;
