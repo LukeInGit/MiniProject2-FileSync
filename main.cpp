2bdfcaa6ea787f047fc76f2857a43d4aa0b069fe
LukeInGit <LukeElias@protonmail.com>
2024-03-01 18:03:15 +0000

    Added a monitordirec function, intended to monitor a direc for changes on a sep thread. currently unused
    added moving and copying file functionality.
    
    sync directories can now sync directories

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <filesystem>
+#include <chrono>
+#include <thread>
+
 
 class DirectoryIterator
 {
@@ -55,19 +58,31 @@ public:
 
 };
 
-bool IsFileInDirectory(const std::string& filename, const std::filesystem::path& directory)         // check if filename is in target directory
+bool FileIsInDirectory(const std::string& filename, const std::filesystem::path& directory)         // check if filename is in target directory
 {
     std::filesystem::path targetFilePath = directory / filename;
     return std::filesystem::exists(targetFilePath);
 }
 
-void SyncDirectories( DirectoryIterator& sourceDirectory,  DirectoryIterator& targetDirectory, bool sourceIsMain) { // compare all filenames between two directories, call relevant copying functions-
+
+
+void CopyFileTo(const std::filesystem::path& filepath, DirectoryIterator& targetDirectory)
+{
+    std::filesystem::copy(filepath, targetDirectory.GetPath());
+}
+
+void MoveFileTo(const std::filesystem::path& filepath, DirectoryIterator& targetDirectory)
+{
+    std::filesystem::rename(filepath, targetDirectory.GetPath()/filepath.filename());
+}
+
+void SyncDirectories( DirectoryIterator& sourceDirectory,  DirectoryIterator& targetDirectory, DirectoryIterator& binDirectory, bool sourceIsMain) { // compare all filenames between two directories, call relevant copying functions-
     for (const auto& entry : sourceDirectory.GetIterator()) {                                    // depending on if filenames exist.
                                                                                                  // bool passed to tell if source is main directory, ideally through directory.IsMainDirectory()
         std::string filename = entry.path().filename().string();   
 
 
-        if (IsFileInDirectory(filename, targetDirectory.GetPath())) {
+        if (FileIsInDirectory(filename, targetDirectory.GetPath())) {
 
                 std::cout << filename << " exists in target directory.\n";
         }
@@ -75,10 +90,12 @@ void SyncDirectories( DirectoryIterator& sourceDirectory,  DirectoryIterator& ta
             if (sourceIsMain)
             {
                 std::cout << filename << " does not exist in target directory. Copying...\n";
+                CopyFileTo(entry.path(), targetDirectory);
             }
             else
             {
                 std::cout << filename << " does not exist in main directory. Deleting...\n";
+                MoveFileTo(entry.path(), binDirectory);
             }
         }
     }
@@ -87,14 +104,40 @@ void SyncDirectories( DirectoryIterator& sourceDirectory,  DirectoryIterator& ta
 }
 
 
-void basicloop(DirectoryIterator& sourceDirectory, DirectoryIterator& targetDirectory) //temporary loop
-{
-    while (true) {
-        SyncDirectories(sourceDirectory, targetDirectory, sourceDirectory.IsMainDirectory());
-        SyncDirectories(targetDirectory, sourceDirectory, targetDirectory.IsMainDirectory());
+void MonitorDirectory(const std::filesystem::path& pathToMonitor, std::stop_token stoken) {                                 //monitor directory and detect changes.
+    std::filesystem::file_time_type lastModificationTime = std::filesystem::last_write_time(pathToMonitor);
 
+    while (!stoken.stop_requested()) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        if (std::filesystem::exists(pathToMonitor)) 
+        {
+            std::filesystem::file_time_type currentModificationTime = std::filesystem::last_write_time(pathToMonitor);
+            if (currentModificationTime != lastModificationTime)
+            {
+                std::cout << "directory has been modified.\n";
+                // perform actions based on directory changes
+                lastModificationTime = currentModificationTime;
+            }
+        }
+        else 
+        {
+            std::cout << "directory does not exist.\n";
+            break;
+        }
     }
 }
+
+
+//void basicloop(DirectoryIterator& sourceDirectory, DirectoryIterator& targetDirectory) //temporary loop
+//{
+//    while (true) {
+//
+//        //SyncDirectories(sourceDirectory, targetDirectory, binDirectory, sourceDirectory.IsMainDirectory());
+//        //SyncDirectories(targetDirectory, sourceDirectory, binDirectory, targetDirectory.IsMainDirectory());
+//
+//    }
+//}
 
 int main()
 {
@@ -103,11 +146,19 @@ int main()
     DirectoryIterator targetDirectory{ "C:/Users/Luke/source/repos/MiniProject_2_FileSync/DirectoryB"};
     DirectoryIterator binDirectory{ "C:/Users/Luke/source/repos/MiniProject_2_FileSync/DirectoryBin"};
 
-    SyncDirectories(sourceDirectory, targetDirectory, sourceDirectory.IsMainDirectory());
-    SyncDirectories(targetDirectory, sourceDirectory, targetDirectory.IsMainDirectory());
+    SyncDirectories(sourceDirectory, targetDirectory, binDirectory, sourceDirectory.IsMainDirectory());
+    SyncDirectories(targetDirectory, sourceDirectory, binDirectory, targetDirectory.IsMainDirectory());
 
     //basicloop(sourceDirectory, targetDirectory);
 
+    //std::string directoryPath = "C:/Users/Luke/source/repos/MiniProject_2_FileSync/DirectoryA";
+
+    //std::stop_source ssource;
+    //std::jthread monitorThread(MonitorDirectory, sourceDirectory.GetPath(), ssource.get_token());
+    //std::jthread monitorThread(MonitorDirectory, targetDirectory.GetPath(), ssource.get_token());
+
+    //std::this_thread::sleep_for(std::chrono::seconds(10));
+    //ssource.request_stop();
 
     return 0;
 }
