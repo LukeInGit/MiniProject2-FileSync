82c854b6b9e650b2ceca127f5b7120193ff9d6a9
LukeInGit <LukeElias@protonmail.com>
2024-03-04 21:36:56 +0000

    Added enum and a struct for directory readability/identification, held with in a vector, planning for scalability with multiple subfolders, functions will need to be modified.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <chrono>
 #include <thread>
+#include <vector>
 
 
 class DirectoryIterator
@@ -11,12 +12,12 @@ private:
     std::filesystem::path m_path{};
     bool m_isMainDirectory{};
 
-    DirectoryIterator() = delete;                                           // prevent default constructer usage
-    DirectoryIterator(const DirectoryIterator&) = delete;                   // delete copy constructor
-    DirectoryIterator& operator=(const DirectoryIterator&) = delete;        // delete assignment constructor
+    //DirectoryIterator() = delete;                                           // prevent default constructer usage
+    //DirectoryIterator(const DirectoryIterator&) = delete;                   // delete copy constructor
+    //DirectoryIterator& operator=(const DirectoryIterator&) = delete;        // delete assignment constructor
 public:
 
-    explicit DirectoryIterator(std::string filepath, bool isMain=false)    
+     DirectoryIterator(std::string filepath, bool isMain=false)    
         :m_iterator{ filepath }, m_path{ filepath }, m_isMainDirectory{isMain}
     {}
 
@@ -58,6 +59,9 @@ public:
 
 };
 
+
+
+
 bool FileIsInDirectory(const std::string& filename, const std::filesystem::path& directory)         // check if filename is in target directory
 {
     std::filesystem::path targetFilePath = directory / filename;
@@ -74,6 +78,22 @@ void CopyFileTo(const std::filesystem::path& filepath, DirectoryIterator& target
 void MoveFileTo(const std::filesystem::path& filepath, DirectoryIterator& targetDirectory)
 {
     std::filesystem::rename(filepath, targetDirectory.GetPath()/filepath.filename());
+}
+
+void DeleteFile(const std::filesystem::path& filepath)
+{
+    std::filesystem::remove(filepath);
+}
+
+void DeleteAllFilesInDirec(DirectoryIterator& binDirectory)
+{
+    for (const auto& entry : binDirectory.GetIterator())
+    {
+        if (std::filesystem::is_regular_file(entry))
+        {
+            DeleteFile(entry.path());
+        }
+    }
 }
 
 void SyncDirectories( DirectoryIterator& sourceDirectory,  DirectoryIterator& targetDirectory, DirectoryIterator& binDirectory, bool sourceIsMain) { // compare all filenames between two directories, call relevant copying functions-
@@ -96,6 +116,7 @@ void SyncDirectories( DirectoryIterator& sourceDirectory,  DirectoryIterator& ta
             {
                 std::cout << filename << " does not exist in main directory. Deleting...\n";
                 MoveFileTo(entry.path(), binDirectory);
+                
             }
         }
     }
@@ -128,7 +149,14 @@ void MonitorDirectory(const std::filesystem::path& pathToMonitor, std::stop_toke
     }
 }
 
-
+namespace DirecType {
+    enum DirectoryType {
+        Bin =0,
+        Main=1,
+        Sub1=2, 
+        MaxDirectories 
+    };
+}
 //void basicloop(DirectoryIterator& sourceDirectory, DirectoryIterator& targetDirectory) //temporary loop
 //{
 //    while (true) {
@@ -139,16 +167,50 @@ void MonitorDirectory(const std::filesystem::path& pathToMonitor, std::stop_toke
 //    }
 //}
 
+struct DirectoryInfo // for making it obvious in the code that a direc is meant to be bin/main/sub
+{
+    std::string name;
+    DirectoryIterator iterator;
+
+    DirectoryInfo(std::string path,  std::string name = "Sub-Directory", bool isMain = false)
+        :  name(name), iterator(path, isMain) {}
+};
+
+
 int main()
 {
+    std::vector<DirectoryInfo> directories=
+    {
+        {"C:/Users/Luke/source/repos/MiniProject_2_FileSync/BinDirectory",      "Bin",      false   },
+        {"C:/Users/Luke/source/repos/MiniProject_2_FileSync/MainDirectory",     "Main",     true    },
+        {"C:/Users/Luke/source/repos/MiniProject_2_FileSync/SubDirectory1",     "Sub"               },
+    };
 
-    DirectoryIterator sourceDirectory{ "C:/Users/Luke/source/repos/MiniProject_2_FileSync/DirectoryA",true };
-    DirectoryIterator targetDirectory{ "C:/Users/Luke/source/repos/MiniProject_2_FileSync/DirectoryB"};
-    DirectoryIterator binDirectory{ "C:/Users/Luke/source/repos/MiniProject_2_FileSync/DirectoryBin"};
 
-    SyncDirectories(sourceDirectory, targetDirectory, binDirectory, sourceDirectory.IsMainDirectory());
-    SyncDirectories(targetDirectory, sourceDirectory, binDirectory, targetDirectory.IsMainDirectory());
+   // DirectoryInfo test{ "C:/Users/Luke/source/repos/MiniProject_2_FileSync/BinDirectory",      "Bin",      false };
 
+    //SyncDirectories(mainDirectory, subDirectory, binDirectory, mainDirectory.IsMainDirectory());
+    //SyncDirectories(subDirectory, mainDirectory, binDirectory, subDirectory.IsMainDirectory());
+
+
+    SyncDirectories(
+        directories[DirecType::Main].iterator,
+        directories[DirecType::Sub1].iterator,
+        directories[DirecType::Bin].iterator,
+        directories[DirecType::Main].iterator.IsMainDirectory()
+    );
+
+    SyncDirectories(
+        directories[DirecType::Sub1].iterator,
+        directories[DirecType::Main].iterator,
+        directories[DirecType::Bin].iterator,
+        directories[DirecType::Sub1].iterator.IsMainDirectory()
+    );
+
+
+    std::cout << directories[DirecType::Bin].name;
+    std::cout << directories[DirecType::Main].name;
+    std::cout << directories[DirecType::Sub1].name;
     //basicloop(sourceDirectory, targetDirectory);
 
     //std::string directoryPath = "C:/Users/Luke/source/repos/MiniProject_2_FileSync/DirectoryA";
