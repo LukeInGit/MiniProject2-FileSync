38a1744e4979125950377f3ef275ecfb6df230ab
LukeInGit <LukeElias@protonmail.com>
2024-03-11 10:30:30 +0000

    removed some old commented code from directoryIterator.h

--- directoryIterator.h
+++ directoryIterator.h
@@ -12,28 +12,17 @@ private:
     std::filesystem::path m_path{};
 public:
 
-    DirectoryIterator(std::string filepath)//, bool isMain = false)
-        :m_iterator{ filepath }, m_path{ filepath }//, m_isMainDirectory{ isMain }
+    DirectoryIterator(std::string filepath)
+        :m_iterator{ filepath }, m_path{ filepath }
     {}
 
-    void ResetIterator()
-    {
-        m_iterator = std::filesystem::directory_iterator{ m_path };
-    }
+    void ResetIterator() { m_iterator = std::filesystem::directory_iterator{ m_path }; }
 
-    const std::filesystem::directory_iterator& GetIterator() const
-    {
-        return m_iterator;
-    }
-
-    const std::filesystem::path& GetPath() const
-    {
-        return m_path;
-    }
+    const std::filesystem::directory_iterator& GetIterator() const {return m_iterator;}
+    const std::filesystem::path& GetPath() const{ return m_path;}
 
     void CheckDirectory()      // print out filenames in directory
     {
-
         for (const auto& entry : m_iterator)
         {
             if (std::filesystem::is_regular_file(entry))
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "imguiGui.h"
 #include "fileManager.h"
-#include <windows.h>
-
+#include <windows.h> //needed if using windows subsystem
 
 //run with console showing up, requires linker>system>subsytem to be console
 int main()
@@ -10,22 +9,12 @@ int main()
 	imguiGUI::imguiMainLoop(__argc, __argv); //currently runs imgui's dx9 example
 }
 
-
-
-
 ////run without console showing up, requires linker>system>subsytem to be windows
 //int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 //{
 //
 //    UNREFERENCED_PARAMETER(hInstance);
 //    UNREFERENCED_PARAMETER(hPrevInstance);
-// 
-// 
-// 
-// 
-// 
-// 
-// 
 //    UNREFERENCED_PARAMETER(lpCmdLine);
 //    UNREFERENCED_PARAMETER(nCmdShow);
 //
